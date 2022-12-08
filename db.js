462155f7e421635cb9eaf3de1df2c3fd9be59737
ronaldarifin <71082114+ronaldarifin@users.noreply.github.com>
2022-12-07 23:40:47 -0800

    fixed local config

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
-// const Pool = require('pg').Pool;
-
-// const pool = new Pool({
-//    user: "postgres",
-//    password: "",
-//    host: "localhost",
-//    port: 5432,
-//    database: "perntodo"
-// })
-
-// module.exports = pool
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-   user: "room_status_db_user",
-   password: "E859GVjs389UEy5pSgUqh7Gg0n33RjKQ",
-   host: "dpg-ce8n5a6n6mposnkvtqh0-a",
+   user: "postgres",
+   password: "",
+   host: "localhost",
    port: 5432,
-   database: "room_status_db"
-});
+   database: "room-status-db"
+})
 
 module.exports = pool
+// const Pool = require('pg').Pool;
+
+// const pool = new Pool({
+//    user: "room_status_db_user",
+//    password: "E859GVjs389UEy5pSgUqh7Gg0n33RjKQ",
+//    host: "dpg-ce8n5a6n6mposnkvtqh0-a",
+//    port: 5432,
+//    database: "room_status_db"
+// });
+
+// module.exports = pool
